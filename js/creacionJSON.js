329cc1991cd47391506168b785be2354d360e327
Samuel Martinez <samuelmtzg798@gmail.com>
2021-09-01 19:08:56 -0500

    Changes | Cambie el fetch pero sigue sin funcionar, me manda un body vacío

--- js/creacionJSON.js
+++ js/creacionJSON.js
@@ -1,10 +1,10 @@
 //Capturamos la carga del formulario para su validacion con javascript
-window.addEventListener('DOMContentLoaded', () => {document.getElementById('form-add-project').addEventListener('submit',validarFormulario);});
+window.addEventListener('DOMContentLoaded', () => { document.getElementById('form-add-project').addEventListener('submit', validarFormulario); });
 
 //Agregamos las funciones para validar el formulario
 
-function validarFormulario(e) { 
-    e.preventDefault(); 
+function validarFormulario(e) {
+    e.preventDefault();
     //Aqui se recogen los valores una vez se envie el formulario
     let projectName = document.getElementById('project_name').value;
     let begin = document.getElementById('begin_date').value;
@@ -17,7 +17,7 @@ function validarFormulario(e) {
         alert('Tu proyecto debe tener un nombre');
         projectName.focus();
         return;
-    }  else if (begin.length == 0){
+    } else if (begin.length == 0) {
         alert('Debes poner una fecha de inicio valida');
         begin.focus();
         return;
@@ -25,7 +25,7 @@ function validarFormulario(e) {
         alert('Debes poner una fecha limite valia');
         end.focus();
         return;
-    }else if (description.length < 50) {
+    } else if (description.length < 50) {
         alert('Ponle una descripcion a tu proyecto de al menos 50 caracteres para brindar una vision más clara de tu proposito');
         description.focus();
         return;
@@ -35,18 +35,19 @@ function validarFormulario(e) {
 
 let imagen = document.getElementById('proyecto-foto');
 let myWidget = cloudinary.createUploadWidget({
-    cloudName: 'rsosar2021', 
-    uploadPreset: 'OsarFotos'}, (error, result) => { 
-      if (!error && result && result.event === "success") { 
-        console.log('Imagen subida con éxito', result.info); 
+    cloudName: 'rsosar2021',
+    uploadPreset: 'OsarFotos'
+}, (error, result) => {
+    if (!error && result && result.event === "success") {
+        console.log('Imagen subida con éxito', result.info);
         imagen.src = result.info.secure_url;
-        }
     }
-  )
-  
-  document.getElementById("upload_widget").addEventListener("click", function(){
-      myWidget.open();
-    }, false);
+}
+)
+
+document.getElementById("upload_widget").addEventListener("click", function () {
+    myWidget.open();
+}, false);
 
 // Function modified to use fetch
 function addProject() {
@@ -54,76 +55,79 @@ function addProject() {
     // Obtenemos los valores del formulario
     let projectName = document.getElementById('project_name').value;
     let projectImg = imagen.src;
-    let leader = 'Elias Flores';
-    console.log(leader);
     let begin = document.getElementById('begin_date').value;
     let end = document.getElementById('end_date').value;
     let description = document.getElementById('description').value;
 
-    const newProject = [{
-        'name': projectName,
-        'fechainicio': begin,
-        'fechatermino': end,
-        'imagen': projectImg,
-        'descripcion': description,
-        'Estatus': 'En progreso',
-        'precioinversion': '100',
-        'totalcorazones': '3',
-        'totalparticipantes': '2',
-        'idusuario': '3',
-        'tipofounding': 'Inversión'
-    }];
-
-    postProject('http://localhost:8080/api/project/', newProject)
+    fetch('http://localhost:8080/api/project/', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            'name': projectName,
+            'fechainicio': begin,
+            'fechatermino': end,
+            'imagen': projectImg,
+            'descripcion': description,
+            'Estatus': 'I',
+            'precioinversion': '100',
+            'totalcorazones': '3',
+            'totalparticipantes': '2',
+            'idusuario': '3',
+            'tipofounding': 'Inversión'
+        })
+    })
         .then(data => {
             console.log('Success: ', data);
         })
         .catch((error) => {
-            console.error('Error: ' , error)
+            console.error('Error: ', error)
         });
-/*
-    if (window.sessionStorage.getItem('projects') === null) {
-        const projectArray = [];
-        const newProject = {
-            'id': 0,
-            'projectName': projectName,
-            'projectImg': projectImg,
-            'leader': leader,
-            'beginDate': begin,
-            'endDate': end,
-            'description': description
-        }
-
-        projectArray.push(newProject);
-        window.localStorage.setItem('projects', JSON.stringify(projectArray));
-    }
-    else {
-        const projectArray = JSON.parse(window.sessionStorage.getItem('projects'));
-        let newId = projectArray.length;
-        const newProject = {
-            'id': newId,
-            'projectName': projectName,
-            'projectImg': projectImg,
-            'leader': leader,
-            'beginDate': begin,
-            'endDate': end,
-            'description': description
-        }
-        
-        projectArray.push(newProject);
-        window.sessionStorage.setItem('projects', JSON.stringify(projectArray));
-*/
-    } // ends project
+    /*
+        if (window.sessionStorage.getItem('projects') === null) {
+            const projectArray = [];
+            const newProject = {
+                'id': 0,
+                'projectName': projectName,
+                'projectImg': projectImg,
+                'leader': leader,
+                'beginDate': begin,
+                'endDate': end,
+                'description': description
+            }
     
-    document.getElementById("form-add-project").reset();//Reiniciamos los valores del formulario
-    Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Proyecto creado '  ,
-        showConfirmButton: false,
-        timer: 1500
-    })
+            projectArray.push(newProject);
+            window.localStorage.setItem('projects', JSON.stringify(projectArray));
+        }
+        else {
+            const projectArray = JSON.parse(window.sessionStorage.getItem('projects'));
+            let newId = projectArray.length;
+            const newProject = {
+                'id': newId,
+                'projectName': projectName,
+                'projectImg': projectImg,
+                'leader': leader,
+                'beginDate': begin,
+                'endDate': end,
+                'description': description
+            }
+            
+            projectArray.push(newProject);
+            window.sessionStorage.setItem('projects', JSON.stringify(projectArray));
+    */
+} // ends project
 
+document.getElementById("form-add-project").reset();//Reiniciamos los valores del formulario
+Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Proyecto creado ',
+    showConfirmButton: false,
+    timer: 1500
+})
+
+/*
 // Fetch POST implementation
 async function postProject(url = '', data = {} ) {
     // Default options are marked with *
@@ -135,4 +139,4 @@ async function postProject(url = '', data = {} ) {
         body: JSON.stringify(data)
     });
     return response.json();
-} // ends postProject function
+} // ends postProject function */
